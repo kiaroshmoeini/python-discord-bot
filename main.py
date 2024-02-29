c0c912f28f752c1cc9d1bf9399d5e55b1eb42027
Kiarosh Moeini <kmoeini@gmail.com>
2024-02-29 14:31:23 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import discord
+
+intents = discord.Intents.all()
+client = discord.Client(command_prefix='!', intents = intents)
+
+@client.event
+async def on_ready():
+    print("We have logged in a s {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith("hi"):
+        await message.channel.send("Welcome, young padawan.")
+
+client.run("MTIxMjU5NDk3OTQ4NzIxOTcxMg.GTFzlN.ohy6GzjbIn_4BOKzLoWCYBv6ZnBqDQRlMZhLX4")
